6c98ba7a267d7183a3799a93ba1ebd21b68efe77
Joe Worsfold <joseph.worsfold@digital.homeoffice.gov.uk>
2023-11-21 19:28:20 +0000

    feat(ch3): types in go

--- golang-book-ch3/main.go
+++ golang-book-ch3/main.go
@@ -3,5 +3,20 @@ package main
 import "fmt"
 
 func main() {
-  fmt.Println("1 + 1 =", 1 + 1)
+	// int and float64
+	fmt.Println("1 + 1 =", 1+1)
+	fmt.Println("1.0 + 1.0 =", 1.0+1.0)
+
+	// strings
+	fmt.Println(len("Hello World"))
+	fmt.Println("Hello World"[1]) // returns e as int representation
+	fmt.Println("Hello " + "World")
+
+	// booleans
+	fmt.Println(true && true)
+	fmt.Println(true && false)
+	fmt.Println(true || true)
+	fmt.Println(true || false)
+	fmt.Println(!true)
+
 }
